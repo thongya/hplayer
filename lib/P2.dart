2dcef04d16d1482f453dc8d9e3e7a2e4e591f1c8
Thongya Murong <thongya@quantummethod.org.bd>
2025-03-18 11:51:53 +0600

    new P2

---
+++ lib/P2.dart
@@ -0,0 +1,6 @@
+void main(){
+  var amount = 100;
+  amount >= 1000 ? print("yes") : amount >= 500 ? print("yes") : amount >= 60 ? print("yes") : print("no");
+
+  Set<int> numbers = {1, 2, 3, 3, 4};
+}
