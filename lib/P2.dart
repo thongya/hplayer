7101a13a7c6fab9fb6c6856f8c31023acf444bd4
Thongya Murong <thongya@engineer.com>
2025-03-18 12:00:12 +0600

    add print

--- lib/P2.dart
+++ lib/P2.dart
@@ -3,4 +3,5 @@ void main(){
   amount >= 1000 ? print("yes") : amount >= 500 ? print("yes") : amount >= 60 ? print("yes") : print("no");
 
   Set<int> numbers = {1, 2, 3, 3, 4};
+  print(numbers);
 }
