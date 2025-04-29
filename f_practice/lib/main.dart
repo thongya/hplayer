877b17d3243269a6f9c77f25f17f887dd81bf3c3
Thongya Murong <thongya@engineer.com>
2025-04-29 10:00:36 +0600

    added email and password field

--- f_practice/lib/main.dart
+++ f_practice/lib/main.dart
@@ -7,12 +7,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginPage(),
-    );
+    return MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage());
   }
 }
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -21,34 +19,57 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   final _formKey = GlobalKey<FormState>();
   String? _email, _password;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Form"),
-      ),
-      body: Padding(padding: const EdgeInsets.all(16.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Email'),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter your email';
+      appBar: AppBar(title: Text("Login Form")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your email';
+                  }
+                  if (!RegExp(r'\S+@\S+\.\S+').hasMatch(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _email = value,
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter your password';
+                  }
+                  if (value.length < 6) {
+                    return 'Password must be at least 6 characters long';
+                  }
+                  return null;
+                },
+                onSaved: (value) => _password = value,
+              ),
+              SizedBox(height: 20),
+              ElevatedButton (onPressed: (){
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save();
+                  print('Email: $_email, Password: $_password');
                 }
-                return null;
-              },
-            )
-          ],
+              }, child: Text('Login'),)
+            ],
+          ),
         ),
-      ),
       ),
     );
   }
