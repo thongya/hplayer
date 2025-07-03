01c68e06ccf7451a67a2fe2ce33c4372e71a15a1
Thongya Murong <thongya@engineer.com>
2025-07-03 12:14:09 +0600

    new UI design

---
+++ task_app/lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(app());
+  }
+  class app extends StatelessWidget {
+  const app({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  return const Placeholder();
+  }
+  }
